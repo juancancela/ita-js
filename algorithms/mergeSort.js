b827d66e7e39662aeef9728ac829d3d97c1a0ba3
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2015-04-25 21:00:28 -0300

    added partial implementation of merge sort algorithm

---
+++ algorithms/mergeSort.js
@@ -0,0 +1,11 @@
+/**
+ * Given an unsorted n-size array, sort it using merge sort
+ * @param unsortedArray an unsorted array
+ */
+function mergeSort(unsortedArray){
+
+}
+
+module.exports = {
+    mergeSort : mergeSort
+};

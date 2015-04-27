714f3639867b54372664632589972f31c6182679
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2015-04-27 00:20:33 -0300

    added mergeSort

--- algorithms/mergeSort.js
+++ algorithms/mergeSort.js
@@ -3,7 +3,44 @@
  * @param unsortedArray an unsorted array
  */
 function mergeSort(unsortedArray){
+    var pivot = Math.floor(unsortedArray.length / 2);
+    if(unsortedArray.length <= 1){
+        return unsortedArray;
+    }
+    var leftArray = mergeSort(unsortedArray.slice(0, pivot));
+    var rightArray = mergeSort(unsortedArray.slice(pivot, unsortedArray.length));
 
+    return _merge(leftArray, rightArray);
+}
+
+/**
+ * Internal function used to sort two arrays
+ * @param unsortedLeftArray an unsorted array
+ * @param unsortedRightArray another unsorted array
+ * @returns {Array} a sorted array
+ * @private
+ */
+function _merge(unsortedLeftArray, unsortedRightArray){
+    var sortedArray = [];
+    while(unsortedLeftArray.length > 0 && unsortedRightArray.length > 0){
+        if (unsortedLeftArray[0] <= unsortedRightArray[0]){
+            sortedArray.push(unsortedLeftArray[0]);
+            unsortedLeftArray.shift();
+        } else {
+            sortedArray.push(unsortedRightArray[0]);
+            unsortedRightArray.shift();
+        }
+    }
+    while(unsortedLeftArray.length > 0){
+        sortedArray.push(unsortedLeftArray[0]);
+        unsortedLeftArray.shift();
+    }
+    while(unsortedRightArray.length > 0){
+        sortedArray.push(unsortedRightArray[0]);
+        unsortedRightArray.shift();
+    }
+
+    return sortedArray;
 }
 
 module.exports = {
--- test/mergeSort.test.js
+++ test/mergeSort.test.js
@@ -1,3 +1,17 @@
-/**
- * Created by bak on 4/25/15.
- */
+var assert = require("assert");
+var mergeSort = require('../algorithms/mergeSort').mergeSort;
+
+describe('Merge Sort', function(){
+    describe('#mergeSort(unsortedArray)', function(){
+        it('should sort [6,5,4,2,1,3] -> [1,2,3,4,5,6]', function(){
+            var unsortedArray = [6,5,4,2,1,3];
+            var sortedArray = mergeSort(unsortedArray);
+            assert.equal(1, sortedArray[0]);
+            assert.equal(2, sortedArray[1]);
+            assert.equal(3, sortedArray[2]);
+            assert.equal(4, sortedArray[3]);
+            assert.equal(5, sortedArray[4]);
+            assert.equal(6, sortedArray[5]);
+        })
+    })
+});

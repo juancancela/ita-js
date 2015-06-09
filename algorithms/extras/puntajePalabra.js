77e11ecedee8ba4762f17ce20f18ef76608ce6e6
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2015-06-08 23:35:36 -0300

    translated extra exercise to eng

--- algorithms/extras/puntajePalabra.js
+++ algorithms/extras/puntajePalabra.js
@@ -1,68 +1,79 @@
 /**
- * Haciendo uso de la posición de las letras en el código ASCII, calculamos el valor de la misma.
- * @param letra la letra sobre la cual se calculara el puntaje
- * @returns {number} the valor de la letra en una serie tal que a = 1, b = 2, ...
- */
-function puntajeLetra(posicion, letra) {
-    var OFFSET_ASCII = 96;
-    return (letra.toLowerCase().charCodeAt(0) - OFFSET_ASCII) * (posicion + 1);
-}
-
-/**
- * Genera las n combinaciones de la palabra dada.
- * @param palabra Palabra usada para generar las combinaciones
- * @returns {Array} Array de combinaciones de la palabra dada.
- */
-function permutator(palabra) {
-    var permutaciones = [];
-
-    function permute(arr, memo) {
-        var actual;
-        var memo = memo || [];
-
-        for (var i = 0; i < arr.length; i++) {
-            actual = arr.splice(i, 1);
-            if (arr.length === 0) {
-                permutaciones.push(memo.concat(actual));
-            }
-            permute(arr.slice(), memo.concat(actual));
-            arr.splice(i, 0, actual[0]);
-        }
-
-        return permutaciones;
-    }
-
-    return permute(palabra);
-}
-
-/**
- * Ejercicio propuesto por sitio hackealo.co
+ * Exercise proposed by hackealo.co
  *
+ * Original (Spanish)
  * El "puntaje" de una palabra es la suma del puntaje de cada letra. El "puntaje" de una letra es la posición en donde
  * se encuentra en una palabra, multiplicada por su posición en el abecedario. Por ejemplo, el puntaje de "acb" es
  * (1*1) + (2*3) + (3*2).
  *
  * ¿Cuál es la suma de las puntuaciones de las permutaciones de "kTBXJJU"?
  *
- * @param palabra palabra de la cual calcularemos su "puntaje"
+ * Translation:
+ *
+ * The "value" of a word is the sum of the value of each of its characters.
+ * The "value" of a character is the product of the position of the character in the work by the position in the alphabet.
+ * IE, the value of "abc" is (1*1) + (2*3) + (3*2)
+ *
  */
-function puntajePalabra(palabra) {
-    if (!palabra) return 0;
-    var puntajePalabra = 0;
-    var permutacionesPalabra = permutator(palabra.split(""));
-    permutacionesPalabra.forEach(function (combinacion) {
-        for (var i = 0; i < combinacion.length; i++) {
-            puntajePalabra += puntajeLetra(i, combinacion[i]);
+
+
+/**
+ * Based on ASCII position, returns the value of a character, taking in consideration the offset.
+ * @param character character to which value will be calculated
+ * @returns {number} value of character
+ */
+function characterValue(position, character) {
+    var OFFSET_ASCII = 96;
+    return (character.toLowerCase().charCodeAt(0) - OFFSET_ASCII) * (position + 1);
+}
+
+/**
+ * Generates the n combinations of the given word
+ * @param word word
+ * @returns {Array} Array with combinations of word
+ */
+function generatePermutations(word) {
+    var permutations = [];
+
+    function permute(arr, memo) {
+        var current;
+        var memo = memo || [];
+
+        for (var i = 0; i < arr.length; i++) {
+            current = arr.splice(i, 1);
+            if (arr.length === 0) {
+                permutations.push(memo.concat(current));
+            }
+            permute(arr.slice(), memo.concat(current));
+            arr.splice(i, 0, current[0]);
+        }
+
+        return permutations;
+    }
+
+    return permute(word);
+}
+
+/**
+ * @param word word de la cual calcularemos su "puntaje"
+ */
+function wordValue(word) {
+    if (!word) return 0;
+    var wordValue = 0;
+    var wordPermutations = generatePermutations(word.split(""));
+    wordPermutations.forEach(function (combination) {
+        for (var i = 0; i < combination.length; i++) {
+            wordValue += characterValue(i, combination[i]);
         }
     });
-    return puntajePalabra;
+    return wordValue;
 }
 
 /**
  * Public Interface
- * @type {{puntajePalabra: puntajePalabra}}
+ * @type {{wordValue: wordValue}}
  */
 module.exports = {
-    puntajePalabra: puntajePalabra
+    wordValue: wordValue
 };
 
--- test/extras/puntajePalabra.test.js
+++ test/extras/puntajePalabra.test.js
@@ -1,18 +1,18 @@
 var assert = require("assert");
-var puntajePalabra = require('../../algorithms/extras/puntajePalabra').puntajePalabra;
+var wordValue = require('../../algorithms/extras/puntajePalabra').wordValue;
 
 describe('El "puntaje" de una palabra es la suma del puntaje de cada letra. El "puntaje" de una letra es la posición en ' +
 'donde se encuentra en una palabra, multiplicada por su posición en el abecedario. Por ejemplo, el puntaje de "acb" es  ' +
 '(1*1) + (2*3) + (3*2).', function(){
     describe('#puntajePalabra(palabra)', function(){
         it('acb == (1*1)+(2*3)+(3*2) => 72', function(){
-            assert.equal(72, puntajePalabra("acb"));
+            assert.equal(72, wordValue("acb"));
         })
     });
 
     describe('#puntajePalabra(palabra)', function(){
         it('kTBXJJU => 1975680', function(){
-            assert.equal(1975680, puntajePalabra("kTBXJJU"));
+            assert.equal(1975680, wordValue("kTBXJJU"));
         })
     })
 });
